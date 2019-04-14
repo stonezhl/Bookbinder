185ea50717b37de1a41619bce4878689c1e9534a
Stone Zhang <stnzhl@gmail.com>
2019-04-14 23:56:54 +0900

    Create initial framework project

---
+++ Bookbinder/Bookbinder/Bookbinder.h
@@ -0,0 +1,19 @@
+//
+//  Bookbinder.h
+//  Bookbinder
+//
+//  Created by Stone Zhang on 4/14/19.
+//  Copyright © 2019 Stone Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bookbinder.
+FOUNDATION_EXPORT double BookbinderVersionNumber;
+
+//! Project version string for Bookbinder.
+FOUNDATION_EXPORT const unsigned char BookbinderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bookbinder/PublicHeader.h>
+
+
